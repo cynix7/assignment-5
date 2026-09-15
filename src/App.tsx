763a6526b61109334a5fcf1c6203e39b15735551
cynix7 <tanvirwd007@gmail.com>
2026-09-15 09:15:05 +0600

    fetched-json-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { Suspense } from "react";
 import Banner from "./components/Banner"
 import Nav from "./components/Nav"
+import Technologies from "./components/Technologies/Technologies";
+import type { TechType } from "./Type/Type";
+
+
+const techPromise = async (): Promise<TechType[]> => {
+ const res=await fetch('/data.json');
+ const data=await res.json();
+ return data;
+}
 
 function App() {
   return (
     <>
       <Nav></Nav>
       <Banner></Banner>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Technologies techPromise={techPromise()}></Technologies>
+      </Suspense>
     </>
   )
 }
---
+++ src/components/Technologies/Technologies.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { use } from 'react';
+import type { TechType } from '../../Type/Type';
+
+const Technologies = ({ techPromise }: { techPromise:Promise<TechType[]> }) => {
+     
+    const technologies = use(techPromise);
+    return (
+        <div className="container mx-auto mt-4 flex flex-col gap-3">
+            <h2 className='text-3xl font-bold'>Explore the <span className='bg-linear-to-r from-[#D81B7E] to-[#8B5CF6] bg-clip-text text-transparent'>Technologies</span></h2>
+            <p className='text-sm text-gray-600'>Pick one technology per category to build your ideal stack.</p>
+            <div className='grid grid-cols-4 gap-4 mt-4'>
+               <div className='col-span-3 grid grid-cols-3 gap-4'>
+                 {technologies.map(tech => (
+                <h2 key={tech.id}>
+                    {tech.name}    
+                </h2>
+            ))}
+               </div>
+               <div>
+                <h2>Your Stack</h2>
+               </div>
+            </div>
+        </div>
+    );
+};
+
+export default Technologies;
